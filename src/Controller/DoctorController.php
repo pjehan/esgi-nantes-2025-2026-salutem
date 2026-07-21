18d1b9b9e87dc77b7e995aa8a225fb4c7a3f523d
pjehan <pierre.jehan@gmail.com>
2026-07-21 09:07:10 +0200

    Add doctor index page and update navigation links to use route paths

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -3,12 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Doctor;
+use App\Repository\DoctorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class DoctorController extends AbstractController
 {
+    #[Route('/doctors', name: 'app_doctors')]
+    public function index(DoctorRepository $doctorRepository): Response
+    {
+        $doctors = $doctorRepository->findAll();
+
+        return $this->render('doctor/index.html.twig', [
+            'doctors' => $doctors,
+        ]);
+    }
+
     #[Route('/doctor/{id}', name: 'app_doctor_show')]
     public function show(Doctor $doctor): Response
     {
