d4d789a48f072df31f310299a622742648181261
pjehan <pierre.jehan@gmail.com>
2026-07-22 08:59:07 +0200

    Ajout de la restriction d'accès aux routes de création de médecin et de spécialité pour les utilisateurs avec le rôle ADMIN

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class DoctorController extends AbstractController
 {
@@ -32,6 +33,7 @@ final class DoctorController extends AbstractController
     }
 
     #[Route('/doctor/new', name: 'app_doctor_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function new(Request $request, EntityManagerInterface $em): Response
     {
         $doctor = new Doctor();
